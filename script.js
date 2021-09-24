eefcb473c9fdfd6a842f63e810438617ec616a8c
Pepe Becker <mail@pepebecker.com>
2021-09-24 09:58:44 +0900

    Use one color with different opacities

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const randInt = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-const createDivider = (color1 = 'white', color2 = 'orange') => {
+const createDivider = (color = 'orange') => {
   const width = 1400;
   const height = 200;
   var wavery = new Wavery({
@@ -10,16 +10,16 @@ const createDivider = (color1 = 'white', color2 = 'orange') => {
     height: height,
     segmentCount: randInt(4, 7),
     layerCount: randInt(4, 5),
-    variance: 0.8 + (randInt(0, 2) / 10), // Decimal value between 0 and 1
+    variance: 0.8 + (randInt(0, 2) / 10),
     strokeWidth: 0,
     strokeColor: 'none',
     gradientColors: [
       {
-        colorValue: color1,
+        colorValue: color,
         position: 0
       },
       {
-        colorValue: color2,
+        colorValue: color,
         position: 1
       }
     ]
@@ -30,19 +30,13 @@ const createDivider = (color1 = 'white', color2 = 'orange') => {
   return svg;
 }
 
-const bodyBackground = getComputedStyle(document.body).backgroundColor
-
-const sections = Array.from(document.querySelectorAll('section'))
-for (const i in sections) {
-  const section = sections[i]
-  const prev = sections[i-1]
-  const fromColor = prev ? getComputedStyle(prev).backgroundColor : bodyBackground
-  const toColor = getComputedStyle(section).backgroundColor
-  section.appendChild(createDivider(fromColor, toColor));
+for (const section of Array.from(document.querySelectorAll('section'))) {
+  const color = getComputedStyle(section).backgroundColor
+  section.appendChild(createDivider(color));
 }
 
 const footer = document.querySelector('footer')
-const lastSectionColor = getComputedStyle(sections[sections.length - 1]).backgroundColor
-footer.appendChild(createDivider(lastSectionColor, bodyBackground));
+const backgroundColor = getComputedStyle(document.body).backgroundColor;
+footer.appendChild(createDivider(backgroundColor));
 
 document.querySelector('.copy-year').textContent = new Date().getFullYear();
